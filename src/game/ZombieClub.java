cc4c21c0f5115db260cd250787b8acaaf6545443
Rahn <rsta0012@student.monash.edu>
2020-05-15 17:38:13 +1000

    Added javadoc for ZombieMace and ZombieClub

--- src/game/ZombieClub.java
+++ src/game/ZombieClub.java
@@ -2,8 +2,16 @@ package game;
 
 import edu.monash.fit2099.engine.WeaponItem;
 
+/**
+ * A weapon made from a zombie arm
+ * @author Rahn Stavar
+ *
+ */
 public class ZombieClub extends WeaponItem{
 
+	/**
+	 * ZombieClub constructor
+	 */
 	public ZombieClub() {
 		super("zombie club", 'c', 30, "clubbed");
 	}
--- src/game/ZombieMace.java
+++ src/game/ZombieMace.java
@@ -2,8 +2,16 @@ package game;
 
 import edu.monash.fit2099.engine.WeaponItem;
 
+/**
+ * A weapon made from a zombie leg
+ * @author Rahn Stavar
+ *
+ */
 public class ZombieMace extends WeaponItem{
 
+	/**
+	 * ZombieMace constructor
+	 */
 	public ZombieMace() {
 		super("zombie mace", 'm', 35, "struck");
 	}
