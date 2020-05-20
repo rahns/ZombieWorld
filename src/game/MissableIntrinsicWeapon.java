11905b7050cabdb135b70b3309310f756d7f1fcf
Arie Hendrikse <ahen0012@student.monash.edu>
2020-05-20 17:07:11 +1000

    Removed hits method and associated imports from missableintrinsicweapn

--- src/game/MissableIntrinsicWeapon.java
+++ src/game/MissableIntrinsicWeapon.java
@@ -1,21 +1,13 @@
 package game;
 
-import java.util.Random;
-
 import edu.monash.fit2099.engine.IntrinsicWeapon;
 
 public class MissableIntrinsicWeapon extends IntrinsicWeapon implements HitProbability {
 	protected int hitProbability;
-	private boolean hits;
-	private Random rand= new Random();
 	
 	public MissableIntrinsicWeapon(int damage, String verb, int hitProbability) {
 		super(damage, verb);
 		this.hitProbability=hitProbability;
-		if (rand.nextInt(100)<hitProbability) {
-			hits=true;
-		}
-		
 	}
 
 
