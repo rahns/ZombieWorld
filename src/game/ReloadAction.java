b455fbb609ca7b4586a40e9c8abdb0664c61a60f
Arie Hendrikse <ahen0012@student.monash.edu>
2020-06-07 20:51:19 +1000

    Added reload action

---
+++ src/game/ReloadAction.java
@@ -0,0 +1,30 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+
+public class ReloadAction extends Action {
+	
+	private AmmunitionCartridge ammo;
+	private SniperRifle gun;
+
+	public ReloadAction(AmmunitionCartridge ammo, SniperRifle gun) {
+		this.gun=gun;
+		this.ammo=ammo;
+	}
+
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		gun.reload(ammo);
+		return null;
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
