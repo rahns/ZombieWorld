abd63e2f9da067c7bdf86ad6f7cfc4fce4423026
Rahn <rsta0012@student.monash.edu>
2020-06-11 12:14:17 +1000

    Simplified EndLifeAction

--- src/game/EndLifeAction.java
+++ src/game/EndLifeAction.java
@@ -11,31 +11,14 @@ public class EndLifeAction extends Action {
 
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		actor.hurt(((ZombieActor) actor).getMaxHitpoints());
-		if (!actor.isConscious()) {
-			boolean shouldRise = false;
-			// Check if target was human and killed by a zombie:
-			if (actor.hasCapability(ZombieCapability.ALIVE) && actor.hasCapability(ZombieCapability.UNDEAD)) {
-				shouldRise = true;
-			}
-			Corpse corpse = new Corpse(actor.toString(), shouldRise, map);
-			map.locationOf(actor).addItem(corpse);
-			
-			Actions dropActions = new Actions();
-			for (Item item : actor.getInventory())
-				dropActions.add(item.getDropAction());
-			for (Action drop : dropActions)		
-				drop.execute(actor, map);
-			map.removeActor(actor);	
-		}
-
+		map.removeActor(actor);
 		return actor+ " took the easy way out ";
 	}
 
 	@Override
 	public String menuDescription(Actor actor) {
 		// TODO Auto-generated method stub
-		return "End it all";
+		return "Quit game";
 	}
 	@Override
 	public String hotkey() {
--- src/game/Wallet.java
+++ src/game/Wallet.java
@@ -1,11 +1,28 @@
 package game;
 
+/**
+ * An interface for objects in the game that can collect and spend coins
+ * @author Rahn Stavar
+ *
+ */
 public interface Wallet {
 
+	/**
+	 * A getter for the object's current number of coins
+	 * @return the number of coins in the wallet
+	 */
 	public int getWealth();
 	
+	/**
+	 * A method to spend an object's coins
+	 * @param cost the number of coins to spend
+	 */
 	public void spendCoins(int cost);
 	
+	/**
+	 * A method for giving a coin to the object
+	 * @param coin a coins
+	 */
 	public void addCoinToWallet(Coin coin);
 	
 }
