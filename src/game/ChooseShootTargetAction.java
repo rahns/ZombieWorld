3bf4e822580752696addd151ee10f8e782b4723a
Rahn <rsta0012@student.monash.edu>
2020-06-12 13:25:07 +1000

    Removed duplicate class

--- src/game/ChooseShootTargetAction.java
+++ src/game/ChooseShootTargetAction.java
@@ -1,15 +1,23 @@
 package game;
 
 import edu.monash.fit2099.engine.Action;
-import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Action that presents a sub menu to choose an actor to shoot at with a sniper.
+ * @author ariehendrikse
+ *
+ */
 public class ChooseShootTargetAction extends Action implements MenuAction {
 
 	private SniperRifle gun;
 
+	/**
+	 * Constructor
+	 * @param sniperRifle - the sniper to be shooting
+	 */
 	public ChooseShootTargetAction(SniperRifle sniperRifle) {
 		this.gun=sniperRifle;
 	}
